8698714eb4839ef98a2c931b47cce344a472625e
Jeffrey Smith <jeffreysmith@vt.edu>
2022-02-26 22:23:30 -0500

    added flask

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+#from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/post', methods=['POST'])
+def post():
+    return "recived: {}".format(request.form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
